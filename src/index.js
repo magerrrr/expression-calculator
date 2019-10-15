439daee9acdc52361bb352181c76fa864c86b626
Kirill Mager <kiruha24@mail.ru>
2019-10-15 18:38:22 +0300

    feat: add substraction

--- src/index.js
+++ src/index.js
@@ -12,18 +12,35 @@ function expressionCalculator(expr) {
     let leftArg = null;
     let rightArg = null;
     let result;
-
+    let operation = '';
+    
     stringFromExpr = expr.replace(/\s+/g, '');
-    arguments = stringFromExpr.split('+');
+    if ( stringFromExpr.indexOf('+') === 1 ) {
+        operation = 'addition';
+        arguments = stringFromExpr.split('+');
+    } else if (stringFromExpr.indexOf('-') === 1) {
+        operation = 'subtraction';
+        arguments = stringFromExpr.split('-');
+    }
     leftArg = Number(arguments[0]);
     rightArg = Number(arguments[1]);
     
-    result = addition(leftArg,rightArg);
+    switch (operation) {
+        case 'addition':
+            result = addition(leftArg,rightArg);
+            break;
+    
+        case 'subtraction':
+            result = subtraction(leftArg,rightArg);
+            break;
+    }
 
     function addition(a,b) {
         return a + b;
     }
-
+    function subtraction(a,b) {
+        return a - b;
+    }
     return result;
     }
 }
